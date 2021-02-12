d8cbf9e8687f94fee72d8722b7eb0774efc1150f
nicholasscabral <nicholasscabral@gmail.com>
2021-02-11 22:51:42 -0300

    initial changes

--- app.js
+++ app.js
@@ -3,13 +3,10 @@ var express = require("express")
 var app = express()
 var router = require("./routes/routes")
  
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
 app.use(bodyParser.json())
-
 app.use("/",router);
 
 app.listen(8686,() => {
-    console.log("Servidor rodando")
+    console.log("API RUNNING...")
 });
