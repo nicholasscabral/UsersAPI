daf93bdb1ff200ad4072e485bd19dbc8cc5097e3
nicholasscabral <nicholasscabral@gmail.com>
2021-02-11 22:44:56 -0300

    config database

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
+
 var knex = require('knex')({
     client: 'mysql2',
     connection: {
-      host : '127.0.0.1',
-      user : 'root',
-      password : '',
-      database : 'usertest'
+      host : process.env.DATABASE_HOST,
+      user : process.env.DATABASE_USER,
+      password : process.env.DATABASE_PASSWORD,
+      database : process.env.DATABASE
     }
   });
 
